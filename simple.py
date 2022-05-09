38b581e22bc1a72e93adc741492d7352c3386b69
norhayatikz <hayatikz87@gmail.com>
2022-05-10 01:24:25 +0800

    initial commit

---
+++ simple.py
@@ -0,0 +1,10 @@
+# this is a very simple python file that prints hello world!. BTW, # in Python means to comment code block
+
+print("hello world!")
+print("this is my first")
+
+x = 5/3
+
+print("this is a variable {}")
+
+print("additional")
